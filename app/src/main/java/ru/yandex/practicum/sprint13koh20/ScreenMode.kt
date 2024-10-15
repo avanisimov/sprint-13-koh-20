7c8cad2443daff0f82e99960d7250ed6247a6385
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2024-10-15 19:25:03 +0300

    #1: Исправлено отображение заголовка в зависимости от выбранной вкладки. Исправления по итогам ревью

--- app/src/main/java/ru/yandex/practicum/sprint13koh20/ScreenMode.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh20/ScreenMode.kt
@@ -1,5 +1,19 @@
 package ru.yandex.practicum.sprint13koh20
 
+import androidx.annotation.StringRes
+
 enum class ScreenMode {
-    CATALOG, CART
+    CATALOG, CART;
+
+    val titleResId: Int
+        @StringRes
+        get() {
+            return when (this) {
+                CATALOG -> R.string.catalog_title
+                CART -> R.string.cart_title
+            }
+        }
+
 }
+
+
